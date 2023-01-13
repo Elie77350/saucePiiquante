30e46970d9cb1a4f9bb9e78bcd02976269302cf7
Elie <eliebingue7@icloud.com>
2023-01-13 18:23:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -13,8 +13,7 @@ app.use(cors());
 
 const { DB_USER, DB_PASSWORD, DB_CLUSTER_NAME } = process.env;
 
-mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER_NAME}.mongodb.net/?retryWrites=true&w=majority`,
-	{ userNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER_NAME}.mongodb.net/?retryWrites=true&w=majority`)
 	.then(() => console.log('successful connection to MongoDB !'))
 	.catch(() => console.log('connection failed to MongoDB !'));
 
@@ -30,13 +29,6 @@ app.use(limiter);
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}));
-
-app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-	next();
-});
 
 app.use('/api/sauces',stuffRoutes);
 app.use('/api/auth', userRoutes);
